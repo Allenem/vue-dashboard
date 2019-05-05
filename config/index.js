f7cdd234a7549bb015f467ed67b63d221c486f1f
Allenem <1820988819@qq.com>
2019-05-05 21:14:47 +0800

    rebuild

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-testing/',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
